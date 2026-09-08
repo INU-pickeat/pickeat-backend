eda5b0a59bcffc74958375a22a8d687d2bec749b
jiin-jung <cki08543@gmail.com>
2026-09-08 17:28:02 +0900

    chore: initialize backend project

---
+++ src/main/java/com/pickeat/pickeatbackend/PickEatBackendApplication.java
@@ -0,0 +1,13 @@
+package com.pickeat.pickeatbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PickEatBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PickEatBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pickeat/pickeatbackend/PickEatBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pickeat.pickeatbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PickEatBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
