1fe14142958e340b16e3d9ad9758989db323abeb
jiin-jung <cki08543@gmail.com>
2026-09-17 13:49:33 +0900

    feat: add Survey entity and Flyway migration
    ERD 설계 문서(surveys 테이블) 기준으로 Survey 엔티티, SurveyCategory enum,
    V4 마이그레이션 추가. creator는 User와 ManyToOne 관계.

---
+++ src/main/java/com/pickeat/pickeatbackend/domain/survey/entity/SurveyCategory.java
@@ -0,0 +1,13 @@
+package com.pickeat.pickeatbackend.domain.survey.entity;
+
+public enum SurveyCategory {
+    CAREER,
+    IT_AI,
+    SERVICE_APP,
+    CONSUMER_MARKETING,
+    GAME,
+    SCHOOL_LIFE,
+    DAILY,
+    PSYCHOLOGY,
+    ETC
+}
